29399a19aaea08d990aeef0c911f119cf89baea1
Frederico Möller <fredericomollerper@gmail.com>
2023-08-07 09:04:44 -0300

    Update MoellerRenderC.cpp

--- CppMRender/MoellerRenderC.cpp
+++ CppMRender/MoellerRenderC.cpp
@@ -1,16 +1,18 @@
-// MoellerRenderC.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <cwchar>
 #include <Windows.h>
 #include <stdlib.h>
+
 
 #define S_WIDTH 21;
 #define S_HEIGHT 21;
 
 unsigned char sCenterX;
 unsigned char sCenterY;
+
+
+
+
 
 void gotoxy(int x, int y) {
     COORD pos = { x, y };
@@ -31,6 +33,7 @@ void print_point_xy(unsigned char x, unsigned char y) {
 
 int main()
 {
+
     sCenterX = S_WIDTH;
     sCenterY = S_HEIGHT;
     sCenterX = sCenterX / 2;
@@ -44,14 +47,3 @@ int main()
 
     gotoxy(0, 15);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
